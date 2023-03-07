d86785b5a1c9fd8ea276752c38e501556c03970c
JADPA-06 <JADPA-06@DESKTOP-BINFC8U>
2023-03-07 10:25:44 -0600

    Agregar archivos de proyecto.

---
+++ EjemploArchivos/fmrEjemploArchivo.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace EjemploArchivos
+{
+    partial class fmrEjemploArchivo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.txtSalida = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(14, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Introduzca el archivo o directorio:";
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtEntrada.Location = new System.Drawing.Point(261, 15);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(476, 23);
+            this.txtEntrada.TabIndex = 1;
+            // 
+            // txtSalida
+            // 
+            this.txtSalida.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtSalida.Location = new System.Drawing.Point(143, 61);
+            this.txtSalida.Multiline = true;
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.Size = new System.Drawing.Size(534, 357);
+            this.txtSalida.TabIndex = 2;
+            this.txtSalida.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSalida_KeyDown);
+            // 
+            // fmrEjemploArchivo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(807, 465);
+            this.Controls.Add(this.txtSalida);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "fmrEjemploArchivo";
+            this.Text = "Ejemplo de Archivos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtEntrada;
+        private TextBox txtSalida;
+    }
+}
---
+++ EjemploArchivos/fmrEjemploArchivo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploArchivos
+{
+    public partial class fmrEjemploArchivo : Form
+    {
+        public fmrEjemploArchivo()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSalida_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                string? nomberFile;
+
+                nomberFile = txtEntrada.Text;
+
+                if(File.Exists(nomberFile))
+                {
+                    txtSalida.Text = GetInformation(nomberFile);
+
+                    try
+                    {
+                        StreamReader sr = new StreamReader(nomberFile);
+                        txtSalida.Text = sr.ReadToEnd();
+                    }
+                    catch (IOException)
+                    {
+
+                        MessageBox.Show("Error al leer el archivo", "Error de archivo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else if(Directory.Exists(nomberFile))
+                {
+                    string[] listDirecctorios;
+                    txtSalida.Text = GetInformation(nomberFile);
+
+                    listDirecctorios = Directory.GetDirectories(nomberFile);
+
+                    txtSalida.Text += "\r\n\r\nContenido del directorio:\r\n";
+
+                    for(int i = 0; i < listDirecctorios.Length; i++)
+                    {
+                        txtSalida.Text += listDirecctorios[i].ToString() + "\r\n";
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(txtEntrada.Text + " no existe ", " Error de arhivo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private string GetInformation(string nomberFile)
+        {
+            string information;
+
+            information = nomberFile + " existe\r\n\r\n";
+
+            information += "Creacion: " + File.GetCreationTime(nomberFile) + "\r\n";
+
+            information += "Ultima modificacion: " + File.GetLastWriteTime(nomberFile) + "\r\n";
+
+            information += "Ultimo acceso: " + File.GetLastAccessTime(nomberFile) + "\r\n" + "\r\n";
+
+
+            return information;
+
+        }
+    }
+}
